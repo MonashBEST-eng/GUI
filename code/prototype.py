d3428df40aa324917131c8a4630f778b601610d7
techy <vmut0005@student.monash.edu>
2026-03-27 19:43:38 +1100

    Added some internal documentation to explain parts that aren't as obvious and TODOs. Started making a logs screen.

--- code/prototype.py
+++ code/prototype.py
@@ -4,19 +4,22 @@ from PyQt6.QtWidgets import (
     QLabel, QPlainTextEdit,
     QPushButton, QDialog, QMessageBox);
 from PyQt6.QtCore import QTimer;
-import pyqtgraph;
+import pyqtgraph; #library responsible for all the plotting
 
    
 
+#Contains the overview window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__();
         
         self.setWindowTitle("TBM Manager");
         
-        overview_layout = QGridLayout();
+        overview_layout = QGridLayout(); #overall grid pattern for all widgets to be organized in
         
+        #stores if the navigation window has been opened, we don't want copies of the same window
         self.navigation = None;
+        self.logs = None; 
         
         
         operation_status_label = QLabel(text="Status: Operationable");
@@ -26,18 +29,23 @@ class MainWindow(QMainWindow):
         proximity_interlock_label = QLabel(text="Proximity Sensors: Ready");
         navigation_status_label = QLabel(text="Navigation Status: Halted");
         
-        logs_textbox = QPlainTextEdit();
-        logs_textbox.setMaximumBlockCount(6);
+        logs_textbox = QPlainTextEdit(); #PlainTextEdit is best for efficient updating for chunks of text like logs
+        logs_textbox.setMaximumBlockCount(6); #this is a log preview, so a limited size is good
         logs_textbox.setReadOnly(True);
         
-        emergency_button = QPushButton();
+        emergency_button = QPushButton(); 
         emergency_button.setText("EMERGENCY STOP!");
         emergency_button.clicked.connect(self.test_dialog);
         
         navigation_open_button = QPushButton();
-        navigation_open_button.setText("Navigation System Open");
+        navigation_open_button.setText("Open Navigation Info");
         navigation_open_button.clicked.connect(self.navigation_open);
+        logs_open_button = QPushButton();
+        navigation_open_button.setText("Open Logs");
+        navigation_open_button.clicked.connect(self.logs_window_open);
         
+        #widget coordinates are provided in (y, x) in a x * y grid
+        #TODO: fix label layout
         overview_layout.addWidget(logs_textbox, 1, 0);
         overview_layout.addWidget(navigation_open_button, 2, 0);
         overview_layout.addWidget(operation_status_label, 0, 1);
@@ -49,24 +57,34 @@ class MainWindow(QMainWindow):
         overview_layout.addWidget(emergency_button, 6,1);
         
         
-        widget = QWidget();
-        widget.setLayout(overview_layout);
+        widget = QWidget(); # windows are empty base widgets, so make a window
+        widget.setLayout(overview_layout); #set the main layout of the window
         
-        self.setCentralWidget(widget);
+    
+        self.setCentralWidget(widget); #make the main screen's pivot widget the window, this is only needed for main window
         
     def test_dialog(self):
-        button = QMessageBox.information(self,"Emergency Stop", "Machine Halted");
+        button = QMessageBox.information(self,"Emergency Stop", "Machine Halted"); #opens an information type message box
     
     def navigation_open(self):
+        #opens the navigation window if it already hasn't been opened
         if self.navigation is None:
             self.navigation = NavigationWindow();
         self.navigation.show();
+        
+    def logs_window_open(self):
+        #opens the navigation window if it already hasn't been opened
+        if self.logs is None:
+            self.logs = NavigationWindow();
+        self.logs.show();
                 
 
+#Window that shows all the navigation info of the TBM
 class NavigationWindow(QWidget):
     def __init__(self):
         super().__init__();
         
+        #TODO: Get test 2D plotting working
         self.test_x = [0, 1, 2, 3, 4, 5, 6];
         self.test_y = [7, 8, 9, 10, 11, 12, 13];
         
@@ -92,7 +110,13 @@ class NavigationWindow(QWidget):
     #    self.test_x.append(self.test_x.__len__() + 1);
     #    self.test_y.append(self.test_x.__len__() + 4);
     #    self.navigation_graph.setData(self.test_x, self.test_y);
+
+#TODO: Complete logs.
+class LogsWindow(QWidget):
+    def __init__(self):
+        super().__init__();
         
+                
 manager_application = QApplication([]);
 main_window = MainWindow();
 main_window.show();
